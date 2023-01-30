aef852361a57329d2559ff1ccd600caefbd2d94b
Siratul Islam <islamsiratul@gmail.com>
2023-01-30 18:25:53 +0600

    admin

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000";
-export const IMG_URL = "http://localhost:8000/uploads/";
+export const API_URL = "https://easy-delivery.onrender.com";
+export const IMG_URL = "https://easy-delivery.onrender.com/uploads/";
